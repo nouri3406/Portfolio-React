eb05726678a60444503b46791e8ba4176f61a924
Nouri3406 <nouri.foudil-bey@epitech.eu>
2026-02-11 02:33:13 +0100

    mise en ligne

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  base: "/Portfolio-React/", 
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    // Si on build pour le déploiement, on utilise le sous-dossier. 
+    // Sinon (en local), on utilise la racine '/'.
+    base: command === 'build' ? '/Portfolio-React/' : '/',
+  }
 })
